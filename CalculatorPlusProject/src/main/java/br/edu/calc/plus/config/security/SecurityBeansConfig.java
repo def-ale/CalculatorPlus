6603f9a2b8680ec7e87537ebbb97fbb2d527767d
Alexandre F Bertolin <alexandrefbertolin@gmail.com>
2025-11-15 03:26:19 -0300

    adicionando tag lazy

--- CalculatorPlusProject/src/main/java/br/edu/calc/plus/config/security/SecurityBeansConfig.java
+++ CalculatorPlusProject/src/main/java/br/edu/calc/plus/config/security/SecurityBeansConfig.java
@@ -2,13 +2,15 @@ package br.edu.calc.plus.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordConfig {
+public class SecurityBeansConfig {
 
     @Bean
+    @Lazy
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
