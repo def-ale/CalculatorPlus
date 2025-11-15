baf3096fa294d5912f493519888b6c58ef32a9c6
Alexandre F Bertolin <alexandrefbertolin@gmail.com>
2025-11-15 03:49:06 -0300

    Adicionando Mock no passwordEncoder para os testes

--- CalculatorPlusProject/src/test/java/br/edu/calc/plus/CalculatorPlus/CalculatorPlusApplicationTests.java
+++ CalculatorPlusProject/src/test/java/br/edu/calc/plus/CalculatorPlus/CalculatorPlusApplicationTests.java
@@ -2,12 +2,16 @@ package br.edu.calc.plus.CalculatorPlus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class CalculatorPlusApplicationTests {
 
+	@MockBean
+	private PasswordEncoder passwordEncoder;
 	@Test
 	void contextLoads() {
 	}
